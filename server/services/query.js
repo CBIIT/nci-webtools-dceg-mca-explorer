c3fdfe570f68068dc5a12e1ecd4a5e2092efe271
Brian Park <park-brian@users.noreply.github.com>
2022-08-28 01:39:40 -0400

    add query service

---
+++ server/services/query.js
@@ -0,0 +1,31 @@
+import knex from "knex";
+import { checkHeartbeat } from "knex-utils";
+
+export function getConnection(filename) {
+  return knex({
+    client: "better-sqlite3",
+    connection: { filename },
+    useNullAsDefault: true,
+  })
+}
+
+export async function getSamples(connection, query) {
+  const { columns, conditions, offset, limit, orderBy } = query;
+
+  let sqlQuery = connection("sample")
+    .select(columns || "*")
+    .offset(offset || 0)
+    .limit(limit || 10000)
+    .orderBy(orderBy || "id");
+
+  for (let condition of conditions || []) {
+    sqlQuery = sqlQuery.where(...condition);
+  }
+
+  return await sqlQuery;
+}
+
+export async function getStatus(connection) {
+  const results = await checkHeartbeat(connection);
+  return results.isOk;
+}
