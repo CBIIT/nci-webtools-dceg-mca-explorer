c059926280c11b9ba3272331d247d318f67c9387
Brian Park <park-brian@users.noreply.github.com>
2022-08-28 01:39:58 -0400

    add express server

---
+++ server/server.js
@@ -0,0 +1,27 @@
+
+import express from "express";
+import { apiRouter } from "./services/api.js";
+import { getConnection } from "./services/query.js";
+import { getLogger } from "./services/logger.js";
+import { logRequests, logErrors } from "./services/middleware.js";
+
+main();
+
+function main(env = process.env) {
+    const app = createApp(env);
+    return app.listen(
+        env.PORT, 
+        () => app.locals.logger.info(`Application is running on port: ${env.PORT}`)
+    );
+}
+
+function createApp(env) {
+    const app = express();
+    app.locals.logger = getLogger("mosaic-tiler", env.LOG_LEVEL);
+    app.locals.connection = getConnection(env.DATABASE_PATH);
+    
+    app.use(logRequests());
+    app.use("/api", apiRouter);
+    app.use(logErrors()); // logErrors should always be last
+    return app;
+}
