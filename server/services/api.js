e5e0236841d47535c958e7bb6bcc87944b2b3bcf
Brian Park <park-brian@users.noreply.github.com>
2022-08-28 01:39:28 -0400

    add api service

---
+++ server/services/api.js
@@ -0,0 +1,20 @@
+import express from "express";
+import Router from "express-promise-router";
+import { getStatus, getSamples } from "./query.js";
+
+export const apiRouter = new Router();
+
+apiRouter.use(express.json());
+
+apiRouter.get("/ping", async (request, response) => {
+    const { connection } = request.app.locals;
+    const status = await getStatus(connection);
+    response.json(status);
+});
+
+apiRouter.post("/query/samples", async (request, response) => {
+    const { connection } = request.app.locals;
+    const query = request.body;
+    const samples = await getSamples(connection, query);
+    response.json(samples);
+});
