c2e2b520b51e2c58b8b9baaf381442dc69d7c993
Judy Yao <xiaozheng.yao@nih.gov>
2023-07-12 16:29:01 -0400

    turn off one log for snp

--- client/src/modules/components/summaryChart/CNV/SnpPlot.js
+++ client/src/modules/components/summaryChart/CNV/SnpPlot.js
@@ -44,7 +44,7 @@ function SnpPlot(props) {
     for (let i = 0; i < results.length; i++) {
       const res = results[i];
       const sp = snparr[i];
-      console.log(res, sp);
+      // console.log(res, sp);
       if (res.doc_count > 0 && sp !== undefined) {
         sp.x = [res.from, res.from];
       }
