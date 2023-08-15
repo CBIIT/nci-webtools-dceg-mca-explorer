abe1d262c34ca26a2d125fffca8815a069a4c9d9
Judy Yao <xiaozheng.yao@nih.gov>
2023-08-15 09:20:52 -0400

    adjust comparison size

--- client/src/modules/components/summaryChart/CNV/CirclePlotTest.js
+++ client/src/modules/components/summaryChart/CNV/CirclePlotTest.js
@@ -102,7 +102,7 @@ export default function CirclePlotTest(props) {
   }, [zoomRange]);
 
   let adjustWidth = 1;
-  let minFigSize = window.innerWidth < 600 ? 450 : 550;
+  let minFigSize = window.innerWidth < 600 ? 450 : 535;
   if (browserSize.width > 1200 && browserSize.width < 1600) adjustWidth = 0.55;
   else if (browserSize.width >= 1600) adjustWidth = 0.48;
   else adjustWidth = 0.7;
